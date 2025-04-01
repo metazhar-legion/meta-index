bfd3ab7b8ca591a6d4142b9db8af40c04c0fe5db
metazhar-legion <metazhar.legion@gmail.com>
2025-04-01 17:10:06 -0400

    added missing file

---
+++ src/mocks/MockToken.sol
@@ -0,0 +1,52 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+
+/**
+ * @title MockToken
+ * @dev Mock ERC20 token for testing
+ */
+contract MockToken is ERC20, Ownable {
+    uint8 private _decimals;
+
+    /**
+     * @dev Constructor
+     * @param name The name of the token
+     * @param symbol The symbol of the token
+     * @param decimals_ The number of decimals for the token
+     */
+    constructor(
+        string memory name,
+        string memory symbol,
+        uint8 decimals_
+    ) ERC20(name, symbol) Ownable(msg.sender) {
+        _decimals = decimals_;
+    }
+
+    /**
+     * @dev Returns the number of decimals used to get its user representation.
+     */
+    function decimals() public view virtual override returns (uint8) {
+        return _decimals;
+    }
+
+    /**
+     * @dev Mints tokens to the specified address
+     * @param to The address to mint tokens to
+     * @param amount The amount of tokens to mint
+     */
+    function mint(address to, uint256 amount) external {
+        _mint(to, amount);
+    }
+
+    /**
+     * @dev Burns tokens from the specified address
+     * @param from The address to burn tokens from
+     * @param amount The amount of tokens to burn
+     */
+    function burn(address from, uint256 amount) external {
+        _burn(from, amount);
+    }
+}
