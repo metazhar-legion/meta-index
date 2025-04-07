3daceb1030cdafabd77ec2ab73e5b651c04bcc90
metazhar-legion <metazhar.legion@gmail.com>
2025-04-07 18:59:35 -0400

    Add tests for vault configuration and asset management methods

--- test/IndexFundVaultV2.t.sol
+++ test/IndexFundVaultV2.t.sol
@@ -15,6 +15,8 @@ import {MockERC20} from "../src/mocks/MockERC20.sol";
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import {CommonErrors} from "../src/errors/CommonErrors.sol";
 import {IAssetWrapper} from "../src/interfaces/IAssetWrapper.sol";
+import {IPriceOracle} from "../src/interfaces/IPriceOracle.sol";
+import {IDEX} from "../src/interfaces/IDEX.sol";
 
 contract IndexFundVaultV2Test is Test {
     // Contracts
@@ -419,6 +421,347 @@ contract IndexFundVaultV2Test is Test {
         assertEq(mockUSDC.balanceOf(address(vault)), assetValue);
         
         // Clear the mocks
+        vm.clearMockedCalls();
+    }
+    
+    function test_SetRebalanceInterval() public {
+        // Initial value should be 1 day
+        assertEq(vault.rebalanceInterval(), 1 days);
+        
+        // Set a new interval (12 hours)
+        uint256 newInterval = 12 hours;
+        vault.setRebalanceInterval(newInterval);
+        
+        // Check the interval was updated
+        assertEq(vault.rebalanceInterval(), newInterval);
+    }
+    
+    function test_SetRebalanceInterval_NonOwner() public {
+        uint256 newInterval = 12 hours;
+        
+        // Should revert when called by non-owner
+        vm.startPrank(user1);
+        vm.expectRevert();
+        vault.setRebalanceInterval(newInterval);
+        vm.stopPrank();
+    }
+    
+    function test_SetRebalanceInterval_TooLarge() public {
+        // Try to set an interval larger than uint32 max
+        uint256 tooLargeInterval = uint256(type(uint32).max) + 1;
+        
+        vm.expectRevert(CommonErrors.ValueTooHigh.selector);
+        vault.setRebalanceInterval(tooLargeInterval);
+    }
+    
+    function test_SetRebalanceThreshold() public {
+        // Default threshold should be 500 basis points (5%)
+        assertEq(vault.rebalanceThreshold(), 500);
+        
+        // Set a new threshold (10%)
+        uint256 newThreshold = 1000;
+        vault.setRebalanceThreshold(newThreshold);
+        
+        // Check the threshold was updated
+        assertEq(vault.rebalanceThreshold(), newThreshold);
+    }
+    
+    function test_SetRebalanceThreshold_NonOwner() public {
+        uint256 newThreshold = 1000;
+        
+        // Should revert when called by non-owner
+        vm.startPrank(user1);
+        vm.expectRevert();
+        vault.setRebalanceThreshold(newThreshold);
+        vm.stopPrank();
+    }
+    
+    function test_SetRebalanceThreshold_TooLarge() public {
+        // Try to set a threshold larger than BASIS_POINTS (10000)
+        uint256 tooLargeThreshold = 10001;
+        
+        vm.expectRevert(CommonErrors.ValueTooHigh.selector);
+        vault.setRebalanceThreshold(tooLargeThreshold);
+    }
+    
+    function test_UpdatePriceOracle() public {
+        // Create a new mock price oracle
+        MockPriceOracle newMockPriceOracle = new MockPriceOracle(address(mockUSDC));
+        
+        // Update the price oracle
+        vault.updatePriceOracle(newMockPriceOracle);
+        
+        // Check the price oracle was updated
+        assertEq(address(vault.priceOracle()), address(newMockPriceOracle));
+    }
+    
+    function test_UpdatePriceOracle_NonOwner() public {
+        MockPriceOracle newMockPriceOracle = new MockPriceOracle(address(mockUSDC));
+        
+        // Should revert when called by non-owner
+        vm.startPrank(user1);
+        vm.expectRevert();
+        vault.updatePriceOracle(newMockPriceOracle);
+        vm.stopPrank();
+    }
+    
+    function test_UpdatePriceOracle_ZeroAddress() public {
+        // Should revert when trying to set zero address
+        vm.expectRevert(CommonErrors.ZeroAddress.selector);
+        vault.updatePriceOracle(IPriceOracle(address(0)));
+    }
+    
+    function test_UpdateDEX() public {
+        // Create a new mock DEX
+        MockDEX newMockDEX = new MockDEX(address(mockPriceOracle));
+        
+        // Update the DEX
+        vault.updateDEX(newMockDEX);
+        
+        // Check the DEX was updated
+        assertEq(address(vault.dex()), address(newMockDEX));
+    }
+    
+    function test_UpdateDEX_NonOwner() public {
+        MockDEX newMockDEX = new MockDEX(address(mockPriceOracle));
+        
+        // Should revert when called by non-owner
+        vm.startPrank(user1);
+        vm.expectRevert();
+        vault.updateDEX(newMockDEX);
+        vm.stopPrank();
+    }
+    
+    function test_UpdateDEX_ZeroAddress() public {
+        // Should revert when trying to set zero address
+        vm.expectRevert(CommonErrors.ZeroAddress.selector);
+        vault.updateDEX(IDEX(address(0)));
+    }
+    
+    function test_IsRebalanceNeeded() public {
+        // Add RWA wrapper to the vault
+        vault.addAsset(address(rwaWrapper), 10000); // 100% weight
+        
+        // Make sure the RWA token can mint
+        mockUSDC.mint(address(rwaSyntheticSP500), 1000000 * 1e6); // Ensure RWA token has USDC
+        
+        // Deposit from user1
+        vm.startPrank(user1);
+        vault.deposit(DEPOSIT_AMOUNT, user1);
+        vm.stopPrank();
+        
+        // Force rebalance to allocate funds
+        vm.warp(block.timestamp + vault.rebalanceInterval() + 1);
+        vault.rebalance();
+        
+        // Initially, no rebalance should be needed since we just rebalanced
+        assertFalse(vault.isRebalanceNeeded());
+        
+        // Mock a significant deviation in asset value (20% higher)
+        uint256 deviatedValue = DEPOSIT_AMOUNT * 120 / 100; // 20% higher
+        vm.mockCall(
+            address(rwaWrapper),
+            abi.encodeWithSelector(IAssetWrapper.getValueInBaseAsset.selector),
+            abi.encode(deviatedValue)
+        );
+        
+        // Now rebalance should be needed (default threshold is 5%)
+        assertTrue(vault.isRebalanceNeeded());
+        
+        // Clear the mock
+        vm.clearMockedCalls();
+    }
+    
+    function test_GetActiveAssets() public {
+        // Initially there should be no active assets
+        address[] memory activeAssets = vault.getActiveAssets();
+        assertEq(activeAssets.length, 0);
+        
+        // Add first asset
+        vault.addAsset(address(rwaWrapper), 5000); // 50% weight
+        
+        // Create and add a second asset
+        RWASyntheticSP500 rwaSyntheticSP500_2 = new RWASyntheticSP500(
+            address(mockUSDC),
+            address(mockPerpetualTrading),
+            address(mockPriceOracle)
+        );
+        
+        StablecoinLendingStrategy stableYieldStrategy2 = new StablecoinLendingStrategy(
+            "Stable Yield 2",
+            address(mockUSDC),
+            address(0x1),
+            address(mockUSDC),
+            address(this)
+        );
+        
+        RWAAssetWrapper rwaWrapper2 = new RWAAssetWrapper(
+            "Second RWA",
+            IERC20(address(mockUSDC)),
+            rwaSyntheticSP500_2,
+            stableYieldStrategy2,
+            mockPriceOracle
+        );
+        
+        rwaSyntheticSP500_2.transferOwnership(address(rwaWrapper2));
+        stableYieldStrategy2.transferOwnership(address(rwaWrapper2));
+        
+        vault.addAsset(address(rwaWrapper2), 3000); // 30% weight
+        
+        // Now there should be two active assets
+        activeAssets = vault.getActiveAssets();
+        assertEq(activeAssets.length, 2);
+        assertTrue(
+            (activeAssets[0] == address(rwaWrapper) && activeAssets[1] == address(rwaWrapper2)) ||
+            (activeAssets[0] == address(rwaWrapper2) && activeAssets[1] == address(rwaWrapper))
+        );
+        
+        // Remove one asset
+        vm.mockCall(
+            address(rwaWrapper),
+            abi.encodeWithSelector(IAssetWrapper.getValueInBaseAsset.selector),
+            abi.encode(0)
+        );
+        vault.removeAsset(address(rwaWrapper));
+        
+        // Now there should be one active asset
+        activeAssets = vault.getActiveAssets();
+        assertEq(activeAssets.length, 1);
+        assertEq(activeAssets[0], address(rwaWrapper2));
+        
+        // Clear the mock
+        vm.clearMockedCalls();
+    }
+    
+    function test_AddAsset_InvalidParams() public {
+        // Test zero address
+        vm.expectRevert(CommonErrors.ZeroAddress.selector);
+        vault.addAsset(address(0), 5000);
+        
+        // Test zero weight
+        vm.expectRevert(CommonErrors.ValueTooLow.selector);
+        vault.addAsset(address(rwaWrapper), 0);
+        
+        // Test adding asset with different base asset
+        MockERC20 differentBaseAsset = new MockERC20("Different", "DIFF", 18);
+        vm.mockCall(
+            address(rwaWrapper),
+            abi.encodeWithSelector(IAssetWrapper.getBaseAsset.selector),
+            abi.encode(address(differentBaseAsset))
+        );
+        
+        vm.expectRevert(CommonErrors.InvalidValue.selector);
+        vault.addAsset(address(rwaWrapper), 5000);
+        
+        // Clear the mock
+        vm.clearMockedCalls();
+        
+        // Test adding same asset twice
+        vault.addAsset(address(rwaWrapper), 5000);
+        vm.expectRevert(CommonErrors.TokenAlreadyExists.selector);
+        vault.addAsset(address(rwaWrapper), 3000);
+        
+        // Test exceeding 100% weight
+        RWASyntheticSP500 rwaSyntheticSP500_2 = new RWASyntheticSP500(
+            address(mockUSDC),
+            address(mockPerpetualTrading),
+            address(mockPriceOracle)
+        );
+        
+        StablecoinLendingStrategy stableYieldStrategy2 = new StablecoinLendingStrategy(
+            "Stable Yield 2",
+            address(mockUSDC),
+            address(0x1),
+            address(mockUSDC),
+            address(this)
+        );
+        
+        RWAAssetWrapper rwaWrapper2 = new RWAAssetWrapper(
+            "Second RWA",
+            IERC20(address(mockUSDC)),
+            rwaSyntheticSP500_2,
+            stableYieldStrategy2,
+            mockPriceOracle
+        );
+        
+        rwaSyntheticSP500_2.transferOwnership(address(rwaWrapper2));
+        stableYieldStrategy2.transferOwnership(address(rwaWrapper2));
+        
+        vm.expectRevert(CommonErrors.TotalExceeds100Percent.selector);
+        vault.addAsset(address(rwaWrapper2), 6000); // 50% + 60% > 100%
+    }
+    
+    function test_RemoveAsset_InvalidParams() public {
+        // Test removing non-existent asset
+        vm.expectRevert(CommonErrors.TokenNotFound.selector);
+        vault.removeAsset(address(rwaWrapper));
+    }
+    
+    function test_UpdateAssetWeight_InvalidParams() public {
+        // Test updating non-existent asset
+        vm.expectRevert(CommonErrors.TokenNotFound.selector);
+        vault.updateAssetWeight(address(rwaWrapper), 7000);
+        
+        // Add asset first
+        vault.addAsset(address(rwaWrapper), 5000);
+        
+        // Test zero weight
+        vm.expectRevert(CommonErrors.ValueTooLow.selector);
+        vault.updateAssetWeight(address(rwaWrapper), 0);
+        
+        // Test exceeding 100%
+        vm.expectRevert(CommonErrors.TotalExceeds100Percent.selector);
+        vault.updateAssetWeight(address(rwaWrapper), 11000); // 110% > 100%
+    }
+    
+    function test_RebalanceWithPaused() public {
+        // Add RWA wrapper to the vault
+        vault.addAsset(address(rwaWrapper), 10000);
+        
+        // Pause the vault
+        vault.pause();
+        
+        // Attempt to rebalance while paused
+        vm.expectRevert(CommonErrors.OperationPaused.selector);
+        vault.rebalance();
+        
+        // Unpause and verify rebalance works
+        vault.unpause();
+        
+        // Force rebalance interval to pass
+        vm.warp(block.timestamp + vault.rebalanceInterval() + 1);
+        
+        // Should not revert
+        vault.rebalance();
+    }
+    
+    function test_HarvestYieldWithPaused() public {
+        // Add RWA wrapper to the vault
+        vault.addAsset(address(rwaWrapper), 10000);
+        
+        // Pause the vault
+        vault.pause();
+        
+        // Attempt to harvest yield while paused
+        vm.expectRevert(CommonErrors.OperationPaused.selector);
+        vault.harvestYield();
+        
+        // Unpause and verify harvest works
+        vault.unpause();
+        
+        // Mock yield harvesting
+        uint256 yieldAmount = 100 * 1e6;
+        vm.mockCall(
+            address(rwaWrapper),
+            abi.encodeWithSelector(IAssetWrapper.harvestYield.selector),
+            abi.encode(yieldAmount)
+        );
+        
+        // Should not revert
+        uint256 harvestedAmount = vault.harvestYield();
+        assertEq(harvestedAmount, yieldAmount);
+        
+        // Clear the mock
         vm.clearMockedCalls();
     }
 }
