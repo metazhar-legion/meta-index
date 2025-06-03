fdd3bcb9635d68d5adb22223fafadddf6d8e81fd
metazhar-legion <metazhar.legion@gmail.com>
2025-06-02 23:08:34 -0400

    test: add initial test setup with simple assertion

---
+++ test/SimpleTest.t.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import {Test} from "forge-std/Test.sol";
+
+/**
+ * @title SimpleTest
+ * @notice A minimal test file that should compile quickly
+ */
+contract SimpleTest is Test {
+    function setUp() public {
+        // Empty setup
+    }
+    
+    function test_Simple() public {
+        // Simple assertion that will always pass
+        assertTrue(true, "This test should always pass");
+    }
+}
